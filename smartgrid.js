50a314ffc1289507b6ab0b07391bfbed27353bf3
Lelouch <flashgrinder92@gmail.com>
2020-07-06 12:23:42 +0400

    Исправил базовые настройки smart-grid

--- smartgrid.js
+++ smartgrid.js
@@ -4,27 +4,34 @@ module.exports = {
 	columns: 12,
 	offset: "30px",
 	container: {
-		maxWidth: "1110px",
+		maxWidth: "1280px",
 		fields: "30px"
 	},
 	breakPoints: {
-	xl: {
-			width: '1200px'
-	},
-	lg: {
-			width: "992px"
-	},
-	md: {
-			width: "768px"
-	},
-	sm: {
-			width: "576px"
-	},
-	xs: {
-			width: "400px",
-			offset: "10px",
-			fields: "5px"
-	}
+        xl: {
+                width: '1200px'
+        },
+        xls: {
+                width: '1024px'
+        },
+        lg: {
+                width: "992px"
+        },
+        md: {
+                width: "768px",
+                offset: "20px",
+                fields: "10px"
+        },
+        sm: {
+                width: "576px",
+                offset: "10px",
+                fields: "5px"
+        },
+        xs: {
+                width: "400px",
+                offset: "10px",
+                fields: "10px"
+        }
 	},
 	mobileFirst: false
 };
